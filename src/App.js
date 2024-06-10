3b8961f135daa6d7c0e9440a4ccb6324e48feb87
João Nunes <joaovitorschafriva@gmail.com>
2024-06-10 19:24:38 -0300

    Adicionando pagina de login

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { CharacterProvider } from './context/CharacterContext';
 import RaceClassPage from './pages/RaceClassPage';
 import AttributePage from './pages/AttributePage';
 import SummaryPage from './pages/SummaryPage';
+import Demo from './pages/LoginPage';
 
 const App = () => {
   return (
     <CharacterProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<RaceClassPage />} />
+          <Route path="/login" element={<Demo />} />
+          <Route path="/raceclass" element={<RaceClassPage />} />
           <Route path="/attributes" element={<AttributePage />} />
           <Route path="/summary" element={<SummaryPage />} />
         </Routes>
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Login from '@react-login-page/base';
+
+const Demo = () => <Login style={{ height: 380 }} />;
+
+export default Demo;
