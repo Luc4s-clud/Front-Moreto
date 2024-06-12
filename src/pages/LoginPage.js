015cde2443b53018fc0219d7a6b3fd82224c93e4
João Nunes <joaovitorschafriva@gmail.com>
2024-06-11 22:57:50 -0300

    Ajustando login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,62 +1,88 @@
-import React, { useState } from 'react';
-    import axios from "../axiosConfig"
+import React, { useState, useEffect } from 'react';
+import axios from "../axiosConfig"
+import UserContext from '../utils/user';
 
-    const Demo = () => {
-      const [login, setLogin] = useState({ username: '', password: '' });
-      const [message, setMessage] = useState('');
+const Demo = () => {
+  const [login, setLogin] = useState({ username: '', password: '' });
+  const [message, setMessage] = useState('');
+  const [userInfo, setUserInfo] = useState({id: '', username: ''});
 
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setLogin({ ...login, [name]: value });
-      };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
 
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          const response = await axios.post('/autenticacao/login', login);
-          if (response.data.type === 'sucess') {
-            console.log(response.data)
-            if (response.data && response.data.token) {
-              localStorage.setItem('access-token', response.data.token);
-              setMessage('Você está logado');
-            }
-          } else {
-            setMessage(response.data.message);
-          }
-        } catch (error) {
-          
-          setMessage(`Ocorreu um erro ao realizar o login!`);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/autenticacao/login', login);
+      console.log(response.data)
+      if (response.data.type === 'success') {
+        if (response.data && response.data.token) {
+          localStorage.setItem('access_token', response.data.token);
+          console.log(localStorage.getItem('access_token'));
+          setMessage('Você está logado');
         }
-      };
+      } else {
+        setMessage(response.data.message);
+      }
+    } catch (error) {
+      setMessage(`Ocorreu um erro ao realizar o login!`);
+    }
+  };
 
-      return (
-        <div style={{ height: 380 }}>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>Usuário:</label>
-              <input
-                type="text"
-                name="username"
-                value={login.username}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Senha:</label>
-              <input
-                type="password"
-                name="password"
-                value={login.password}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <button type="submit">Login</button>
-          </form>
-          {message && <p>{message}</p>}
-        </div>
-      );
-    };
+  const token = localStorage.getItem('access_token');
 
-    export default Demo;
+  useEffect(() => {
+    if (token) {
+      axios.get('http://localhost:3333/autenticacao/user', {
+        headers: {
+          'Authorization': `${token}`
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+        setUserInfo(response.data.data);
+      })
+      .catch(error => {
+        console.error('Erro ao obter informações do usuário', error);
+      });
+    } else {
+      console.error('Token de acesso não encontrado no localStorage');
+    }
+  }, [token]);
+
+  return (
+    <UserContext.Provider value={userInfo}>
+      <div style={{ height: 380 }}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Usuário:</label>
+            <input
+              type="text"
+              name="username"
+              value={login.username}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Senha:</label>
+            <input
+              type="password"
+              name="password"
+              value={login.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        {message && <p>{message}</p>}
+        {userInfo && <p>Bem-vindo, {userInfo.username}!</p>}
+      </div>
+    </UserContext.Provider>
+  );
+};
+
+export default Demo;
